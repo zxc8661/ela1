75dacd516b8f01248ee43aac2d2bec96e2cd931b
jhs512 <jangka512@gmail.com>
2025-04-02 12:08:23 +0900

    work

---
+++ readme.md
@@ -0,0 +1,3 @@
+# 1강
+- [커밋](https://github.com/jhs512/kp-2025-04-02/commit/main)
+- 프로젝트 생성
---
+++ src/test/kotlin/com/back/Kp20250402ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Kp20250402ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
